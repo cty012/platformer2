4b0b96a4c4c128beec1ee361c8db624a357b724d
cty012 <tianyue.cao21@gmail.com>
2020-09-26 09:57:41 +0800

    Font changed
    Changed font from cheri to UA squared;
    Simplified level 10's elevator track

--- utils/fonts.py
+++ utils/fonts.py
@@ -1,5 +1,5 @@
 def get_font(font_name, font_size):
-    return eval(font_name)(font_size)
+    return 'src', f'{font_name}.ttf', font_size
 
 
 # times new roman
@@ -12,9 +12,6 @@ def cambria(font_size):
     return 'src', 'cambria.ttf', font_size
 
 
-def cheri(font_size):
-    return 'src', 'cheri.ttf', font_size
-
-
+# digital
 def digital_7(font_size):
     return 'src', 'digital-7-mono.ttf', font_size
